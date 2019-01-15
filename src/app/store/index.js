45422d66476e07efb160f9939e4045680317b682
Daniel Ortiz Lira <d.ortiz.lira@gmail.com>
2019-01-15 16:21:21 -0500

    add redux-form

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import getReducers from 'app/utils/getReducers';
 
-const rootReducer = combineReducers(getReducers());
+const rootReducer = combineReducers({ ...getReducers(), form: formReducer });
 
 const storeWithDevToolsConfig = composeWithDevTools(
   applyMiddleware(thunk)
