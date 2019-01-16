6e021e5ee3585fb82dcb93e809f0c8032d5a7b21
Daniel Ortiz Lira <d.ortiz.lira@gmail.com>
2019-01-16 11:42:47 -0500

    add home page

---
+++ src/app/pages/Home/elements.js
@@ -0,0 +1,14 @@
+import { styled } from 'styletron-react';
+
+export const Title = styled('h1', {
+  fontWeigth: '500',
+  fontSize: '1.5em',
+  textAlign: 'center',
+  margin: '20px'
+});
+
+export const ButtonWrapper = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  margin: '20px'
+});
--- src/app/pages/Home/index.js
+++ src/app/pages/Home/index.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import objectToQueryString from 'app/utils/objectToQueryString';
+import Map from 'app/components/ui/Map';
+import Button from 'app/components/ui/Button';
+import { Title, ButtonWrapper } from './elements';
 
-const Home = () => (
-  <div>
-    <h1> Home page </h1>
-  </div>
-);
+const DEFAULT_CENTER = {
+  lat: -34.9209098815918,
+  lng: -56.150474548339844
+};
+
+class Home extends Component {
+  state = { lat: null, lng: null };
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { coords: { latitude: lat, longitude: lng } } = position;
+        this.setCenter({ lat, lng });
+      });
+    } else {
+      this.setCenter(DEFAULT_CENTER);
+    }
+  }
+
+  setCenter = ({ lat, lng }) => {
+    this.setState({ lat, lng });
+  }
+
+  search = () => {
+    const { history } = this.props;
+    const { lat, lng } = this.state;
+    const query = objectToQueryString({ lat, lng });
+    history.push(`/results${query}`);
+  }
+
+  render() {
+    const { lat, lng } = this.state;
+    return (
+      <div>
+        <Title>
+          Search for restaurants near to you!
+        </Title>
+        <Map
+          handleClick={this.setCenter}
+          center={{ lat, lng }}
+          zoom={14}
+        />
+        <ButtonWrapper>
+          <Button onClick={this.search}>
+            Search
+          </Button>
+        </ButtonWrapper>
+      </div>
+    );
+  }
+}
 
 export default Home;
